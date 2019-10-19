5bbaea7f33b874706ebb55a25d2125108b30c3e6
SAM-H <srhardin94@gmail.com>
2019-10-18 17:33:01 -0700

    Better project structure for future workflow

---
+++ proj_6/js/interface/LineGraph.js
@@ -0,0 +1,91 @@
+var margin = {top: 50, right: 50, bottom: 50, left: 50}
+width = 960;
+height = 500;
+  
+var csvData;
+d3.csv("https://raw.githubusercontent.com/srhardin/cs599/master/board_games_clean01.csv").then(function(data) 
+{
+  data.forEach(function(d) 
+  {
+      d.average_rating = +d.average_rating;
+      d.min_age = +d.min_age;
+  });
+  
+  csvData = data;
+  AfterLoad();
+});
+
+function AfterLoad()
+{
+    // Monstrosity. Nasty. Bleh!!!
+    var agedData = d3.nest()
+    .key(function(d) 
+    {
+        return d.min_age;
+        })
+    .rollup(function(leaves) 
+    {
+        return d3.median(leaves, function(d) 
+            {
+                return +d.average_rating;
+            });
+    })
+    .sortKeys((a,b) => d3.ascending(+a, +b))
+    .entries(csvData);
+    
+    // 5. X scale will use the index of our data
+    var xScale = d3.scaleLinear()
+    .domain([0, 25]) // input
+    .range([0, width]); // output
+
+    // 6. Y scale will use the randomly generate number 
+    var yScale = d3.scaleLinear()
+    .domain([0, 10]) // input 
+    .range([height, 0]); // output 
+
+    // 7. d3's line generator
+    var line = d3.line()
+    .x(function(d) { return xScale(d.key); }) // set the x values for the line generator
+    .y(function(d) { return yScale(d.value); }) // set the y values for the line generator 
+
+    // 1. Add the SVG to the page and employ #2
+    var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    // 3. Call the x axis in a group tag
+    svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(xScale)); // Create an axis component with d3.axisBottom
+    
+    // text label for the y axis
+    svg.append("text")
+      .attr("y", (height + margin.bottom - 25))
+      .attr("x", (width/2))
+      .attr("dy", "1em")
+      .style("text-anchor", "middle")
+      .text("Minimum Age to Play");
+
+    // 4. Call the y axis in a group tag
+    svg.append("g")
+    .attr("class", "y axis")
+    .call(d3.axisLeft(yScale)); // Create an axis component with d3.axisLeft
+    
+    // text label for the y axis
+    svg.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left)
+      .attr("x", 0 - (height / 2))
+      .attr("dy", "1em")
+      .style("text-anchor", "middle")
+      .text("Average Rating");
+
+    // 9. Append the path, bind the data, and call the line generator 
+    svg.append("path")
+    .datum(agedData) // 10. Binds data to the line 
+    .attr("class", "line") // Assign a class for styling 
+    .attr("d", line); // 11. Calls the line generator 
+}
