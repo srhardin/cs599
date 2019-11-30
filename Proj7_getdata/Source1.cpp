0ca62d3a2eb28469debd91fdd3535d65a3586b39
chunyanli <emilyusa@yahoo.com>
2019-11-29 16:06:56 -0800

    indentation of method removeComments

--- Proj7_getdata/Source1.cpp
+++ Proj7_getdata/Source1.cpp
@@ -13,27 +13,46 @@ vector<string> removeComments(vector<string>& source) {
 	vector<string> ans;
 	bool inBlock = false;
 	string sf;
-	for (auto &t : source) {
-		for (int i = 0; i < t.size();) {
-			if (!inBlock) {
-				if (i + 1 == t.size()) sf += t[i++];
-				else {
+	for (auto &t : source) 
+	{
+		for (int i = 0; i < t.size();) 
+		{
+			if (!inBlock) 
+			{
+				if (i + 1 == t.size()) 
+					sf += t[i++];
+				else 
+				{
 					string m = t.substr(i, 2);
-					if (m == "/*") inBlock = true, i += 2;
-					else if (m == "//") break;
-					else sf += t[i++];
+					if (m == "/*") 
+					{
+						inBlock = true;
+						i += 2;
+					}
+					else if (m == "//") 
+						break;
+					else 
+						sf += t[i++];
 				}
 			}
-			else {
-				if (i + 1 == t.size()) i++;
-				else {
+			else 
+			{
+				if (i + 1 == t.size()) 
+					i++;
+				else 
+				{
 					string m = t.substr(i, 2);
-					if (m == "*/") inBlock = 0, i += 2;
+					if (m == "*/") 
+						inBlock = 0, i += 2;
 					else i++;
 				}
 			}
 		}
-		if (sf.size() && !inBlock) ans.push_back(sf), sf = "";
+		if (sf.size() && !inBlock) 
+		{
+			ans.push_back(sf);
+			sf = "";
+		}
 	}
 	return ans;
 }
