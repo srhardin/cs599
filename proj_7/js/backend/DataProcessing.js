2e94c692ec1bf423ab3b0e1c0daf52ce82050b80
sgoodsle7 <56521585+sgoodsle7@users.noreply.github.com>
2019-12-03 18:09:10 -0800

    Add files via upload
    Added other graph

--- proj_7/js/backend/DataProcessing.js
+++ proj_7/js/backend/DataProcessing.js
@@ -8,7 +8,7 @@
 
 function getData()
 {
-    d3.csv("https://raw.githubusercontent.com/srhardin/cs599/master/proj_7/data/data.csv").then(function(data)
+d3.csv("https://raw.githubusercontent.com/srhardin/cs599/master/proj_7/data/data.csv").then(function(data)
     {
         for (var i = 0; i < data.length; i++) 
         {
@@ -41,6 +41,62 @@ function getData()
         
         console.log("Done loading data...");
         UpdateGraphs();
+		
+				console.log(data);
+		
+		//something like this
+		
+		// set the dimensions and margins of the graph
+		var margin = {top: 20, right: 20, bottom: 120, left: 40},
+			width = 850 - margin.left - margin.right,
+			height = 575 - margin.top - margin.bottom;
+		
+		// set the ranges
+		var x = d3.scaleBand()
+				.range([0, width])
+				.padding(0.1);
+		var y = d3.scaleLinear()
+				.range([height, 0]);
+				
+		// append the svg object to the body of the page
+		// append a 'group' element to 'svg'
+		// moves the 'group' element to the top left margin
+		var svg = d3.select("#accountValue").append("svg")
+			.attr("width", width + margin.left + margin.right)
+			.attr("height", height + margin.top + margin.bottom)
+		.append("g")
+			.attr("transform", 
+				"translate(" + margin.left + "," + margin.top + ")");
+		
+		
+		// Scale the range of the data in the domains
+		x.domain(data.map(function(d) { return d.fileName; }));
+		y.domain([0, d3.max(data, function(d) { return d.code; })]);
+		
+		// append the rectangles for the bar chart
+		svg.selectAll(".bar")
+			.data(data)
+			.enter().append("rect")
+			.attr("class", "bar")
+			.attr("x", function(d) { return x(d.fileName); })
+			.attr("width", x.bandwidth())
+			.attr("y", function(d) { return y(d.code); })
+			.attr("height", function(d) { return height - y(d.code); });
+		
+		// add the x Axis
+		svg.append("g")
+			.attr("transform", "translate(0," + (height) +  ")")
+			.call(d3.axisBottom(x))
+		.selectAll("text")
+			.attr("y", 0)
+			.attr("x", 9)
+			.attr("dy", ".35em")
+			.attr("transform", "rotate(90)")
+			.style("text-anchor", "start");
+		
+		// add the y Axis
+		svg.append("g")
+			.call(d3.axisLeft(y));
     });
 }
 
